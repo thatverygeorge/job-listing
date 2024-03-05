9dd4266850ed56cdbf08d7e4fd3abe6fa12d9af5
George Kirakosyan <georgiikirakosyan@mail.ru>
2024-03-05 16:29:26 +0300

    introduce footer

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,36 @@
+<template>
+  <footer>
+    <div class="container">
+      <h2>
+        made by
+        <a href="https://github.com/thatverygeorge" target="_blank" rel="noopener noreferrer">
+          @thatverygeorge
+        </a>
+      </h2>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  background-color: var(--green);
+}
+
+.container {
+  min-height: 10rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 2rem;
+}
+
+footer h2 {
+  text-align: end;
+  font-size: 2rem;
+  color: var(--white);
+}
+
+footer a {
+  color: currentColor;
+}
+</style>
