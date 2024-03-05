26e7a4a4b1a3b475d77c36c32c66838e66ebf1b8
George Kirakosyan <georgiikirakosyan@mail.ru>
2024-03-05 16:29:05 +0300

    introduce header

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <div class="container">
+      <h1>Dream Job</h1>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  background-color: var(--green);
+}
+
+.container {
+  min-height: 10rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 2rem;
+}
+
+h1 {
+  font-size: 3rem;
+  color: var(--white);
+}
+</style>
